a580e6cb9e0e059d03433257952d7d0e8f191efc
razor23gtk <razorclaw996@gmail.com>
2025-02-24 20:17:42 +0000

    Add files via upload

--- commands/lavalink/mplay.js
+++ commands/lavalink/mplay.js
@@ -26,11 +26,11 @@ const { autoplayCollection } = require('../../mongodb');
 const { playlistCollection } = require('../../mongodb');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Music')
-        .setDescription('Music Play commands.')
+        .setName('music')
+        .setDescription('Music player commands.')
         .addSubcommand(subcommand =>
             subcommand
-                .setName('Play')
+                .setName('play')
                 .setDescription('Play a song or playlist in the voice channel.')
                 .addStringOption(option =>
                     option.setName('query')
